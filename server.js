1f38fe8e977e025c532355fd541d60e50a4fc7b6
aakash <aakashbist890@gmail.com>
2022-09-28 18:40:10 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express=require('express')
+const app=express()
+const mongoose=require('mongoose')
+const ShortUrl=require('./model/shortUrl')
+
+mongoose.connect('mongodb://localhost/urlShortener', {useNewUrlParser:true, useUnifiedTopology:true})
+
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+
+app.get('/', async(req, res)=>{
+    const shortUrls=await ShortUrl.find()
+    res.render('index', {shortUrls:shortUrls})
+})
+
+app.post('/shortUrls', async(req, res)=>{
+    await ShortUrl.create({full: req.body.fullUrl})
+
+
+    res.redirect('/')
+})
+
+app.listen(process.env.PORT|| 5000)
